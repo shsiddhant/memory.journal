2b90194528c9c925697433139fd59c0b4c6c78e1
Siddhant Sharma <sharmasiddhant@protonmail.com>
2025-12-17 17:41:59 +0530

    bug: fix csrf token missing - generate database and secrets key on first run. (#2)

--- src/memoryjournal/__init__.py
+++ src/memoryjournal/__init__.py
@@ -9,28 +9,24 @@ if TYPE_CHECKING:
 
 from . import db, auth, journal
 
+csrf = CSRFProtect()
 
-BASE_DIR = user_data_path(appname="memoryjournal", appauthor=False, ensure_exists=True)
-JOURNALS_DIR = BASE_DIR / "journals"
-CONFIG_FILE = BASE_DIR / "config.py"
-DATABASE = BASE_DIR / "memoryjournal.sqlite"
-if not DATABASE.exists():
-    db.init_db()
-
-if not CONFIG_FILE.exists():
-    DEFAULT_CONFIG = f"""
+def create_app(testconfig: Mapping | None = None):
+    BASE_DIR = user_data_path(
+        appname="memoryjournal",
+        appauthor=False,
+        ensure_exists=True
+    )
+    CONFIG_FILE = BASE_DIR / "config.py"
+    DATABASE = BASE_DIR / "memoryjournal.sqlite"
+    if not CONFIG_FILE.exists():
+        DEFAULT_CONFIG = f"""
 SESSION_TIMEOUT = 30 * 60  # 30 minutes
 SECRET_KEY = '{secrets.token_hex(32)}'
 """
-    CONFIG_FILE.write_text(DEFAULT_CONFIG)
-
-
-csrf = CSRFProtect()
-
-
-def create_app(testconfig: Mapping | None = None):
+        CONFIG_FILE.write_text(DEFAULT_CONFIG)
     app = Flask(__name__, instance_path=str(BASE_DIR), instance_relative_config=True)
-    app.config.from_mapping(DATABASE=BASE_DIR / "memoryjournal.sqlite")
+    app.config.from_mapping(DATABASE=DATABASE)
     if testconfig is None:
         app.config.from_pyfile(CONFIG_FILE, silent=True)
     else:
@@ -41,6 +37,9 @@ def create_app(testconfig: Mapping | None = None):
         return render_template("index.html")
 
     db.init_app(app)
+    with app.app_context():
+        if not DATABASE.exists():
+            db.init_db()
     csrf.init_app(app)
     app.register_blueprint(auth.bp)
     app.register_blueprint(journal.bp)
@@ -52,3 +51,4 @@ def create_app(testconfig: Mapping | None = None):
         return {"journals": journals}
 
     return app
+
