2c579f53f952223146a7e92452eb4913a99914a4
Siddhant Sharma <sharmasiddhant@protonmail.com>
2025-12-17 16:58:19 +0530

    bug: fix csrf token missing - generate database and secrets key on first run.

--- src/memoryjournal/__init__.py
+++ src/memoryjournal/__init__.py
@@ -4,22 +4,29 @@ from flask import Flask, render_template
 from flask_wtf.csrf import CSRFProtect
 import secrets
 from platformdirs import user_data_path
-
-BASE_DIR = user_data_path(appname="memoryjournal", appauthor=False, ensure_exists=True)
-JOURNALS_DIR = BASE_DIR / "journals"
-csrf = CSRFProtect()
-
 if TYPE_CHECKING:
     from collections.abc import Mapping
 
 from . import db, auth, journal
 
+csrf = CSRFProtect()
 
 def create_app(testconfig: Mapping | None = None):
-    app = Flask(__name__, instance_path=str(BASE_DIR), instance_relative_config=True)
-    app.config.from_mapping(
-        SECRET_KEY=secrets.token_hex(16), DATABASE=BASE_DIR / "memoryjournal.sqlite"
+    BASE_DIR = user_data_path(
+        appname="memoryjournal",
+        appauthor=False,
+        ensure_exists=True
     )
+    CONFIG_FILE = BASE_DIR / "config.py"
+    DATABASE = BASE_DIR / "memoryjournal.sqlite"
+    if not CONFIG_FILE.exists():
+        DEFAULT_CONFIG = f"""
+SESSION_TIMEOUT = 30 * 60  # 30 minutes
+SECRET_KEY = '{secrets.token_hex(32)}'
+"""
+        CONFIG_FILE.write_text(DEFAULT_CONFIG)
+    app = Flask(__name__, instance_path=str(BASE_DIR), instance_relative_config=True)
+    app.config.from_mapping(DATABASE=DATABASE)
     if testconfig is None:
         app.config.from_pyfile("config.py", silent=True)
     else:
@@ -30,6 +37,9 @@ def create_app(testconfig: Mapping | None = None):
         return render_template("index.html")
 
     db.init_app(app)
+    with app.app_context():
+        if not DATABASE.exists():
+            db.init_db()
     csrf.init_app(app)
     app.register_blueprint(auth.bp)
     app.register_blueprint(journal.bp)
@@ -41,3 +51,4 @@ def create_app(testconfig: Mapping | None = None):
         return {"journals": journals}
 
     return app
+
